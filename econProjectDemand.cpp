2ad5447fc397f6010012efe81b3f2f2b23473768
BrenyaDickens <brenya.r.dickens@gmail.com>
2025-03-30 11:27:21 -0500

    Add files via upload

---
+++ econProjectDemand.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+
+// intro, verse and explaination: 
+cout << "*******************************************************************************************************************" << endl;
+cout << "***************************************** ECON SUPPLY & DEMAND CURVE **********************************************" << endl;
+cout << "*******************************************************************************************************************" << endl << endl; 
+cout << "Verse from Rockstar by Nickelback 'Cause we all just wanna be big rockstars, and live in hilltop houses, dirving 15 cars...'" << endl << endl;
+cout << "The line from this song represents a shift in demand for hilltop houses: "<< endl << endl;
+
+cout << "P | (Pre-Shift) * (Post-Shift) *                         \n";
+cout << "  |  *         *            *                            \n";
+cout << "  |   *          *         *                             \n";
+cout << "  |    * --->     *      *                               \n";
+cout << "  |     *           *  *                                 \n";
+cout << "  |------*---------- *                                   \n";
+cout << "  |       *       *  | *                                 \n";
+cout << "  |        *  *      |   *                               \n";
+cout << "  |-------* *        |     *                             \n";                                    
+cout << "  |    *  |   * ---> |       *                           \n";
+cout << "  | *     |    *     |         *                         \n";
+cout << "  |---------------------------------------- Q            \n";
+
+}
